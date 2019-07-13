15e8e8c8e750e1299244ead55bd105361d3b6971
Jorge <jpineda@unimagdalena.edu.co>
2019-07-13 15:59:59 -0500

    Crear persona terminado 2

--- app/src/main/java/com/example/personasmaterial/AgregarPersona.java
+++ app/src/main/java/com/example/personasmaterial/AgregarPersona.java
@@ -3,6 +3,10 @@ package com.example.personasmaterial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -10,6 +14,7 @@ import java.util.Random;
 public class AgregarPersona extends AppCompatActivity {
 
     private ArrayList<Integer> fotos;
+    private EditText nombre, apellido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,32 @@ public class AgregarPersona extends AppCompatActivity {
         fotos.add(R.drawable.images2);
         fotos.add(R.drawable.images3);
 
+        nombre = findViewById(R.id.txtNombre);
+        apellido = findViewById(R.id.txtApellido);
+
+    }
+
+    public void guardar(View v){
+        String nom, apell, id;
+        int foto;
+        id = Datos.getId();
+        nom = nombre.getText().toString();
+        apell = apellido.getText().toString();
+        foto = this.fotoAleatoria();
+        Persona p = new Persona(id, foto, nom, apell);
+        p.guardar();
+        limpiar();
+        Snackbar.make(v,"Persona guardada exitosamente",Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void limpiar(View v){
+        limpiar();
+    }
+
+    public void limpiar(){
+        nombre.setText("");
+        apellido.setText("");
+        nombre.requestFocus();
     }
 
     public int fotoAleatoria(){
